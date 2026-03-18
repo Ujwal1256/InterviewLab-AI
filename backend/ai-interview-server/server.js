8e0b964dc8a00b553dc1557aa7a268764bc70cc1
Ujwal1256 <jakhamateujwal1256@gmail.com>
2026-03-18 11:18:47 +0530

    backend setup and mongodb atlas is connected

---
+++ backend/ai-interview-server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db")
+
+dotenv.config();  // configure dotenv
+connectDB();  // connect DB 
+
+const app = express();
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/",(req,res) => {
+    res.send("API is running ...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,() => {
+    console.log(`Server running on port ${PORT}`);
+})
+
